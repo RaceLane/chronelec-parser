53c7540b8e4dd3211854ab1f98041ffdbcda9b11
Rodewitsch <rodewitsch@inbox.ru>
2023-12-26 17:20:22 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Elite
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        string BinFile = "file.bin";
+        Console.WriteLine("BIN file: " + BinFile);
+        //Format the object as Binary  
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.AssemblyFormat = FormatterAssemblyStyle.Simple;
+        //Reading the file from the server  
+        FileStream fs = File.Open(BinFile, FileMode.Open);
+   
+        object obj = formatter.Deserialize(fs);
+        fs.Flush();
+        fs.Close();
+        fs.Dispose();
+        }
+    }
+}
